697703f7946eb010f74858710d6f83d000bb8083
Khang nè <hkhang1111111100000000@gmail.com>
2025-10-10 22:40:53 +0700

    Push code FeatureLogin lần 2

--- Areas/Admin/Controllers/CategoryController.cs
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using BaiTap_03_23WebC_Nhom10.Filters;//Bổ sung thêm namspace để dùng filter áa
 namespace BaiTap_03_23WebC_Nhom10.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [AuthorizeRole("Admin")]//Bổ sung hạn chế chỉ có admin mới có thể gọi controller nì 
     [Route("Admin/[Controller]")]
     public class CategoryController : Controller
     {
-        
+
         public IActionResult Index()
         {
             return View();
